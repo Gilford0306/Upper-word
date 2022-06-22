5c51fdf5cbfa8bfa0af955c2c151b6ae8db46283
Gilford0306 <Gilford0306@gmail.com>
2022-06-22 20:40:17 +0300

    Add project files.

---
+++ Upper word/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задание 6
+//Пользователь с клавиатуры вводит некоторый текст. 
+//Приложение должно изменять регистр первой буквы 
+//каждого предложения на букву в верхнем регистре.
+Console.WriteLine("Input string");
+string m = Console.ReadLine();
+string[] words = m.Split(new char[] { '.' });
+for (int i = 0; i < words.Length; i++)
+{
+    string temp = words[i];
+    for (int j = 0; j < temp.Length; j++)
+    {
+        if (Char.IsLetter(temp[j]))
+        {
+            if (j > 0)
+            {
+                temp = temp.Substring(0, j)+ char.ToUpper(temp[j]) + temp.Substring(j + 1);
+                break;
+            }
+            temp = char.ToUpper(temp[j]) + temp.Substring(j+1);
+            break;
+        }
+    }
+    if (i == (words.Length - 1))
+    { 
+        Console.Write(temp);
+        return;
+    }
+    Console.Write(temp+".");
+}
